825557e4521d673e70a34bbc2573c782c383fcc3
Michael Bauer <michael.bauer@sf.frb.org>
2018-09-24 18:02:04 -0700

    ESS simplified

--- init-ess.el
+++ init-ess.el
@@ -1,8 +1,8 @@
 ;; ESS
-;;(require 'ess-site) ;; this takes a while, doesn't seem necessary
+(require 'ess-site) ;; this takes a while, doesn't seem necessary
 (setq inferior-R-args "--no-save ")
-(when (string-equal (system-name) "L1MYB00L4")
-  (setq inferior-R-program-name "C:/Program Files/R/R-3.4.2/bin/x64/Rterm"))
+;; (when (string-equal (system-name) "L1MYB00L5")
+;;   (setq inferior-R-program-name "C:/Program Files/R/R-3.5.0/bin/x64/Rterm"))
 ;; (setq inferior-STAT-program-name "C:/Program Files (x86)/Stata14/StataMP-64.exe")
 ;; (setq inferior-julia-program-name "C:/Users/Michael/AppData/Local/Julia-0.6.0/bin/julia.exe")
 ;; (setq inferior-julia-args "-L essfix.jl")
