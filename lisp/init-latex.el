7e64cdfef16322a5786bd6eca378bac64e689125
Michael Bauer <michael.d.bauer@gmail.com>
2020-07-14 22:11:57 +0200

    undoing a change with tex viewer auctex config

--- lisp/init-latex.el
+++ lisp/init-latex.el
@@ -70,13 +70,13 @@ Frame must be declared as an environment."
 
 (if (string-equal system-type "windows-nt")
     (setq TeX-view-program-list
-          (if (string-equal (system-name) "UN-WKS-009761")
-              '(("Sumatra PDF" ("\"C:/Program Files/SumatraPDF/SumatraPDF.exe\" -reuse-instance"
-                                (mode-io-correlate " -forward-search %b %n ") " %o")))
-            '(("Sumatra PDF" ("\"C:/Program Files (x86)/SumatraPDF/SumatraPDF.exe\" -reuse-instance"
-                              (mode-io-correlate " -forward-search %b %n ") " %o")))))
-          ;; '(("Sumatra PDF" ("\"C:/Program Files (x86)/SumatraPDF/SumatraPDF.exe\" -reuse-instance"
-          ;;                   (mode-io-correlate " -forward-search %b %n ") " %o"))))
+          ;; (if (string-equal (system-name) "UN-WKS-009761")
+          ;;     '(("Sumatra PDF" ("\"C:/Program Files/SumatraPDF/SumatraPDF.exe\" -reuse-instance"
+          ;;                       (mode-io-correlate " -forward-search %b %n ") " %o")))
+          ;;   '(("Sumatra PDF" ("\"C:/Program Files (x86)/SumatraPDF/SumatraPDF.exe\" -reuse-instance"
+          ;;                     (mode-io-correlate " -forward-search %b %n ") " %o")))))
+          '(("Sumatra PDF" ("\"C:/Program Files/SumatraPDF/SumatraPDF.exe\" -reuse-instance"
+                            (mode-io-correlate " -forward-search %b %n ") " %o"))))
   (setq TeX-view-program-list (quote (("Okular" "okular --unique %o#src:%n%b")))))
 
 ;; (string-equal (system-name) "UN-WKS-009761") "C:/Dropbox")
